91fc384066617ec1272f363ba4adef010cfb800d
heretycka <aherecka@gmail.com>
2018-03-18 18:10:06 +0100

    commit topics

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ const GITHUB = require('./config').GITHUB;
 
 const hello = require('hellojs');
 
+const topics = new Set();
 
 
 
@@ -63,7 +64,61 @@ function renderUserDetails(userProfile) {
     console.log(template);
 };
 
+const $form = document.querySelector('.js-form-add-topic');
+$form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
 
+    const data = new FormData($form);
+    const map = new Map(data.entries());
+    console.log(map);
+
+    topics.add(map)
+
+    $form.reset();
+
+    renderTopics();
+});
+
+$form.addEventListener('reset', (evt) => {
+
+
+
+})
+
+function renderTopics() {
+    var all = '';
+
+    topics.forEach((topic) => {
+        const topicTemplate = ` <div class="column is-3">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            ${topic.get('topic')}
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            ${topic.get('description')}
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="#" class="card-footer-item">Zagłosuj</a>
+                        <a href="#" class="card-footer-item">Chcę być trenerem</a>
+                    </footer>
+                </div>
+            </div>`;
+
+
+        all += topicTemplate;
+
+    });
+
+    console.log(all);
+
+    var dataAll = document.querySelector('#topicsAll');
+    dataAll.innerHTML = all;
+
+}
 
 
 
