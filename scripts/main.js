394da5067d5e3b494e50d9fc96332e5b5e62702a
heretycka <aherecka@gmail.com>
2018-03-18 15:42:27 +0100

    log in commit

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+﻿require('bulma');
+const hello = require('hellojs');
+
+const client_id = '01dc9f362f800af8f4b4';
+
+
+
+hello.init({
+    github: '01dc9f362f800af8f4b4'
+});
+
+var buttonLog = document.querySelector('#log');
+buttonLog.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    hello('github').login()
+        .then(function () {
+            return hello('github').api('/me');
+        })
+        .then(function (userProfile) {
+            
+            renderUserDetails(userProfile);
+
+        });
+});
+
+var buttonLogOut = document.querySelector('#logOut');
+
+
+function renderUserDetails(userProfile) {
+
+    const template = `<div class="navbar-item">
+                ${userProfile.login}
+                <img src="${userProfile.avatar_url}" alt="avatar"/>
+            </div>`;
+
+    var user = document.querySelector('#bar');
+    user.innerHTML+= template;
+
+    console.log(template);
+};
+
+
+
+
+
